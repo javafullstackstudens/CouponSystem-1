cb73bc96b6e1cf81214c156dc5a2cc0f8c06523b
OrielBasson <orielbasson@gmail.com>
2019-02-01 07:56:09 +0200

    Singleton DB + Singleton ConnPool created.

--- CouponSystem/src/Java/DB/ConnPool.java
+++ CouponSystem/src/Java/DB/ConnPool.java
@@ -1,5 +1,16 @@
 package Java.DB;
 
 public class ConnPool {
+	
+	private static ConnPool instance = new ConnPool();
+	
+	private ConnPool() {
+	}
+	
+	public static ConnPool getInstance() {
+		return instance;
+	}
+	
+	
 
 }
